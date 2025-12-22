3e81385b71ae8f34763d7dee8660d70971cb4b9a
mnjames <m____s@sil.___>
2025-12-22 13:56:57 +0200

    2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1479484068"
+    serviceWorkerVersion: "1693292085"
   }
 });
